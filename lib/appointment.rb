155640d7f5f32a548384bb37634790f56d211013
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2017-11-02 16:17:30 +0000

    automatically backed up by learn

---
+++ lib/appointment.rb
@@ -0,0 +1,13 @@
+class Song
+
+  #Variables
+  attr_accessor :name, :patient
+
+
+  def initialize(name, patient)
+    @name = name
+    @patient = patient
+    self.patient.add_appointment(self)
+  end
+
+end
